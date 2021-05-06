a338b7da7bf6089b2b0fd0f626c06d4feb78e5cd
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-05-06 22:52:13 +0530

    Add files via upload

---
+++ use of variable.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int var1;
+    int var2;
+    var1=10;
+    var2=20;
+    printf("Value of first variable is: %d \n",var1);
+    printf("Value of second variable is: %d",var2);
+    return 0;
+}
