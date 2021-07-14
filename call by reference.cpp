12b6a9a83a518fa6dcf90aef5585c9be601243f9
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-07-14 22:06:00 +0530

    Add files via upload

---
+++ call by reference.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void changeValue(int* address)
+{
+	*address = 345;
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+int main(){
+	int a=34,b=76;
+	printf("the value of a now is %d\n",a);
+	changeValue(&a);
+	printf("the value of a now is %d\n",a);
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+}
