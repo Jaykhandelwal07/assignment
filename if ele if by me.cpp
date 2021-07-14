a62204e5646dd5c4a6771e1443fff8ed8ed67679
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-07-14 21:31:20 +0530

    Add files via upload

---
+++ if ele if by me.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main()
+{
+	char maths ;
+	char science ;
+	
+	printf("student paseed in subjects\n");
+	 scanf("%c\n & %c\n",&maths , &science);
+	
+		
+     if (maths){
+     	printf("students will be awarded 15 rs");
+	 }		
+		
+	else if (science){
+		
+		printf("stutents will be awrded 15 rs");
+	}	
+	
+	else  if(maths and science){
+	
+	printf("students will be awarded 45 rs");
+		}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ if statement.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main () {
+
+  
+   int a = 10;
+ 
+	
+   if( a < 20 ) {
+      /* if condition is true then print the following */
+      printf("a is less than 20\n" );
+   }
+   
+   printf("value of a is : %d\n", a);
+ 
+   return 0;
+}
