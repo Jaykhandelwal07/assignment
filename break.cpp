88a188517f04f5dc8630ca7d48f1238ae5a5f263
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-07-14 21:17:11 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	printf("hello world\n");
+	int i, age ;
+	 for(i=0; i<10; i++){
+		
+		printf("%d \nEnter your age",i);
+	
+		scanf("%d",&age);
+	if(age>10){
+		
+		break;
+	}
+	
+	
+}
+	return 0;
+	
+	
+}
