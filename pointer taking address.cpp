a339f18a5e874473a8e22f2b5e994ae702d919e7
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-07-17 21:35:47 +0530

    Add files via upload

---
+++ pointer taking address.cpp
@@ -0,0 +1,18 @@
+  
+#include<stdio.h>
+int main()
+{
+ int i=8;
+ int *j;
+
+ j=&i;
+ 
+ printf("Value of i is %d \n",i);
+ printf("Value of if() is %d\n",*(&i));
+ printf("Value of i is %d\n",*j);
+ printf("Value of j is %d\n",j);
+ printf("Address of i is %p\n",&i);
+ printf("Address of i is %p\n",j);
+ printf("Address of j is %p\n",&j);
+ return 0;
+}
