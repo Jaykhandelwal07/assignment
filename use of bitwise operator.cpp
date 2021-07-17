8a9bb8d5e04cea628fb56ac558c817c976e65636
JAY KHANDELWAL <83777047+Jaykhandelwal07@users.noreply.github.com>
2021-07-17 21:20:35 +0530

    Add files via upload

---
+++ use of bitwise operator.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("my name is  jay khandelwal\n");
+    printf("Using of BITWISE operator. \n");
+    printf("Enter the value of a and b:");
+    scanf("%d %d",&a,&b);
+    //Using BITWISE operator
+    a&b; //using bitwise AND
+    printf("\nThe value of a AND b is %d",a&b);
+    a|b; // using bitwise OR
+    printf("\nThe value of a OR b is %d",a|b);
+    a^b; //using bitwise XOR
+    printf("\nThe value of a XOR b is %d",a^b);
+    return 0;
+}
